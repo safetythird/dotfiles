7fa473594ec0383d3d44f381280c92eee34d6bb0
Ben Greenberg <bgreenberg85@gmail.com>
2023-03-19 13:31:44 -0700

    Add ruby and comment to treesitterconfig ensure_installed

--- nvim/lua/treesitterconfig.lua
+++ nvim/lua/treesitterconfig.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-	ensure_installed = { "c", "lua", "vim", "help", "python", "javascript" },
+	ensure_installed = { "c", "lua", "vim", "help", "python", "javascript", "ruby", "comment" },
 	highlight = { enable = true },
 	indent = { enable = true}
 }
