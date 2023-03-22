37840053a77d453891f4cac685cdf99ad97b17fd
Ben Greenberg <bgreenberg85@gmail.com>
2023-03-22 09:02:22 -0700

    treesitter: add svelte; enable autotag and endwise

--- nvim/lua/treesitterconfig.lua
+++ nvim/lua/treesitterconfig.lua
@@ -1,5 +1,7 @@
 require'nvim-treesitter.configs'.setup {
-	ensure_installed = { "c", "lua", "vim", "help", "python", "javascript", "ruby", "comment" },
+	ensure_installed = { "c", "lua", "vim", "help", "python", "javascript", "ruby", "comment", "svelte" },
 	highlight = { enable = true },
-	indent = { enable = true}
+	indent = { enable = true},
+  autotag = { enable = true },
+  endwise = { enable = true },
 }
