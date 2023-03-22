cf73a4e6650596f0fb034c3f52593c71a729de33
Ben Greenberg <bgreenberg85@gmail.com>
2023-03-22 09:01:47 -0700

    nvim-tree: Set update_focused_file to true

--- nvim/lua/treeconfig.lua
+++ nvim/lua/treeconfig.lua
@@ -1,6 +1,13 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-require("nvim-tree").setup({
-	open_on_setup = true
+local nvimtree = require("nvim-tree")
+
+nvimtree.setup({
+	open_on_setup = true,
+  update_focused_file = {
+    enable = true,
+    update_root = false
+  }
 })
+
