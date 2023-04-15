0dfa43f9c6d6824e2cbe88c26221142b483d798e
Ben Greenberg <bgreenberg85@gmail.com>
2023-04-15 10:26:12 -0700

    Add markdown support to treesitterconfig; reformat for readability

--- nvim/lua/treesitterconfig.lua
+++ nvim/lua/treesitterconfig.lua
@@ -1,7 +1,25 @@
-require'nvim-treesitter.configs'.setup {
-	ensure_installed = { "c", "lua", "vim", "help", "python", "javascript", "ruby", "comment", "svelte", "go", "html", "sql" },
-	highlight = { enable = true },
-	indent = { enable = true},
+require 'nvim-treesitter.configs'.setup {
+  ensure_installed = {
+    "markdown",
+    "markdown_inline",
+    "c",
+    "lua",
+    "vim",
+    "help",
+    "python",
+    "javascript",
+    "ruby",
+    "comment",
+    "svelte",
+    "go",
+    "html",
+    "sql"
+  },
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = { "markdown" }
+  },
+  indent = { enable = true },
   autotag = { enable = true },
   endwise = { enable = true },
 }
