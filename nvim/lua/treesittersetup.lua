f49d0f0f0a84bf59035da75dd619d2c39233d557
Ben Greenberg <bgreenberg85@gmail.com>
2023-04-23 22:09:35 -0700

    Set up fancy treesitter motions/selects

--- nvim/lua/treesittersetup.lua
+++ nvim/lua/treesittersetup.lua
@@ -22,4 +22,66 @@ require 'nvim-treesitter.configs'.setup {
   indent = { enable = true },
   autotag = { enable = true },
   endwise = { enable = true },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "<leader>v",
+      node_incremental = "<leader>v",
+      scope_incremental = "grc",
+      node_decremental = "<leader>V",
+    },
+  },
+  textobjects = {
+    select = {
+      enable = true,
+      lookahead = true,
+      keymaps = {
+        ["am"] = "@function.outer",
+        ["im"] = "@function.inner",
+        ["ac"] = "@class.outer",
+        ["ic"] = "@class.inner",
+        ["ab"] = "@block.outer",
+        ["ib"] = "@block.inner",
+        ["al"] = "@loop.outer",
+        ["il"] = "@loop.inner",
+        ["ap"] = "@parameter.outer",
+        ["ip"] = "@parameter.inner"
+      }
+    },
+    swap = {
+      enable = true,
+      swap_next = { ["<leader>a"] = "@parameter.inner" },
+      swap_previous = { ["<leader>A"] = "@parameter.inner" }
+    },
+    move = {
+      enable = true,
+      set_jumps = true,
+      goto_next_start = {
+        ["]m"] = "@function.outer",
+        ["]]"] = "@class.outer",
+        ["]b"] = "@block.outer",
+        ["]p"] = "@parameter.inner"
+      },
+      goto_next_end = {
+        ["]M"] = "@function.outer",
+        ["]["] = "@class.outer",
+        ["]B"] = "@block.outer"
+      },
+      goto_previous_start = {
+        ["[m"] = "@function.outer",
+        ["[["] = "@class.outer",
+        ["[b"] = "@block.outer",
+        ["[p"] = "@parameter.inner"
+      },
+      goto_previous_end = {
+        ["[M"] = "@function.outer",
+        ["[]"] = "@class.outer",
+        ["[B"] = "@block.outer"
+      }
+    },
+  },
 }
+
+local ts_repeat_move = require "nvim-treesitter.textobjects.repeatable_move"
+vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
+vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
