21bbfdf67a2eb6e8af4c6f9a31fccdbbcc591a4b
Ben Greenberg <bgreenberg85@gmail.com>
2024-10-27 07:36:37 -0700

    Change tsserver to ts_ls to satisfy deprecation warning

--- nvim/lua/lspsetup.lua
+++ nvim/lua/lspsetup.lua
@@ -2,7 +2,7 @@ local lspconfig = require('lspconfig')
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 local lsputil = require('lspconfig/util')
 -- https://github.com/typescript-language-server/typescript-language-server
-lspconfig['tsserver'].setup {
+lspconfig['ts_ls'].setup {
 	capabilities = capabilities
 }
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#svelte
